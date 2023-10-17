c48878d9e7600476abc1f0ab5ba278c06f41dbd2
Javier-David-Jimenez <142829587+Javier-David-Jimenez@users.noreply.github.com>
2023-10-17 22:20:27 +0200

    colve carro probles in carro.py and context_processor
    para solucionar los problemas e que no reconoce el carro descomentamos carro y el context_processor.
    
    en context processor yo he copiado en el else el codigo y sigue sumando aunque no este logueado
    
    el propone un mensaje de alerta aunque parece que puedes seguir metiendo cosas al carro y cuando loguees puedes encontrar una sorpresa.
    
    se ve obligado a abrir carro en el views de proyecto_Web_App para que siempre este abierto cuando entremos aunque no logueemos

--- Proyecto_Web/ProyectoWebApp/views.py
+++ Proyecto_Web/ProyectoWebApp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponse
- 
+from carro.carro import Carro
 # Create your views here.
 
 def home(request):
+    
+    carro = Carro(request)
     return render(request, "ProyectoWebApp/home.html")
 
 
 
-
+"""creamos carro aqui al principio porque da problemas si no haces los cambios que hice yo para que 
+ siempre se viera el carro estes logueado o no"""
